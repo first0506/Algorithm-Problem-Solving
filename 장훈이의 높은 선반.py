e7d14087e7127573961792cc163dac2b2e176612
김현준 <first0506@naver.com>
2020-06-05 12:13:05 +0900

    combination

--- 장훈이의 높은 선반.py
+++ 장훈이의 높은 선반.py
@@ -13,3 +13,21 @@ for test_case in range(1, T+1):
         if ans == 0:
             break
     print('#{} {}'.format(test_case, ans))
+
+from itertools import combinations
+T = int(input())
+for test_case in range(1, T+1):
+    N, B = map(int, input().split())
+    h = list(map(int, input().split()))
+    ans = 2**31-1
+    for k in range(1, N+1):
+        tmp = combinations(h, k)
+        for i in tmp:
+            s = sum(i)
+            if s>=B and s-B<ans:
+                ans = s-B
+            if not ans:
+                break
+        if not ans:
+            break
+    print('#{} {}'.format(test_case, ans))
